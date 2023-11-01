7ed07a28878de61bfe35b6f1d3acaf2af986270f
Vinicius <viniciusr.camargo@gmail.com>
2023-11-01 11:35:29 -0300

    Inserção dos exercícios 7 e 8 da lista

---
+++ 24102023 - introdução ao javascript/Exercicios resolvidos/verificanum.js
@@ -0,0 +1,7 @@
+var num = -10;
+
+if (num > 0) {
+    console.log("Número positivo");
+} else {
+    console.log("Número negativo");
+}
---
+++ 24102023 - introdução ao javascript/Exercicios resolvidos/verificasalario.js
@@ -0,0 +1,7 @@
+var salario = 1500;
+
+if (salario > 1320) {
+    console.log("Salário maior que o mínimo");
+}else{
+    console.log("Salário menor que o mínimo");
+}
