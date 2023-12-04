a19f25d4cccb89799603b780cf9166b0baa823a5
Vinicius <viniciusr.camargo@gmail.com>
2023-12-04 20:55:28 -0300

    Inclusão do arquivo js da página Register

--- 28112023 - Implementando um sistema de login simples/sistemaLogin/js/register.js
+++ 28112023 - Implementando um sistema de login simples/sistemaLogin/js/register.js
@@ -1,4 +1,5 @@
 const msgAlerta = document.querySelector(".msgAlerta");
+const email = document.getElementById("email");
 const senha = document.getElementById("senha");
 const confirmaSenha = document.getElementById("confimaSenha");
 
@@ -9,8 +10,27 @@ function registrar(){
           msgAlerta.innerHTML = '<h3 class="alerta">As senhas não conicidem!</h3>';
           msgAlerta.removeAttribute('hidden')
           setInterval(() => {
-            msgAlerta.setAttribute('hidden', true); 
+            msgAlerta.setAttribute('hidden', true);
+            limpar(); 
           },4000)  
-          
+          return       
       } 
+      let dados = {
+          email: email.value,
+          senha: senha.value,
+          confirmaSenha: confirmaSenha.value
+      }
+      localStorage.setItem('dados', JSON.stringify(dados));
+      limpar()
+      alert('Cadastro realizado com sucesso!');
+      setInterval(() => {        
+        window.location.href = 'index.html';
+    },2000)
+    
+ }
+
+ function limpar(){
+    senha.value = '';
+    confirmaSenha.value = '';
+    senha.focus();
  }
